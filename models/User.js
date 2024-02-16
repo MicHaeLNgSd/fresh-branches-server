16fe06e9a5f422b54f998a60904991f449623f85
MicHaeLNgSd <misha.grigorov2020@gmail.com>
2024-02-16 23:28:13 +0200

    Finish User model CRUD

--- models/User.js
+++ models/User.js
@@ -1,4 +1,4 @@
-const users = [{ id: 1 }, { id: 2 }];
+let users = [{ id: 1 }];
 
 class User {
   static async create(newData) {
@@ -9,12 +9,31 @@ class User {
     users.push(newUser);
     return newUser;
   }
-  static async findOne() {}
+
+  static async findOne(id) {
+    const user = users.find((u) => u.id === id);
+    return user;
+  }
+
   static async findAll() {
     return users;
   }
-  static async update() {}
-  static async delete() {}
+
+  static async update(id, newData) {
+    users = users.map((u) => {
+      if (u.id !== id) return u;
+      const updatedUser = { ...u, ...newData };
+      return updatedUser;
+    });
+    const updatedUser = await User.findOne(id);
+    return updatedUser;
+  }
+
+  static async delete(id) {
+    const deletedUser = await User.findOne(id);
+    users = users.filter((u) => u.id !== id);
+    return deletedUser;
+  }
 }
 
 module.exports = User;
