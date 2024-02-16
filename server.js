dbb380d9a68ef43ba127d2712ef3d369da5cf24e
MicHaeLNgSd <misha.grigorov2020@gmail.com>
2024-02-16 17:05:01 +0200

    express get

--- server.js
+++ server.js
@@ -1,38 +1,31 @@
-const http = require('http');
+const express = require('express');
+const app = express();
+
+const PORT = 3000;
+const HOST = 'localhost';
 
 const users = [{ id: 1 }, { id: 2 }];
 
-const server = http.createServer((req, res) => {
-  // console.log(req, res)
-
-  if (req.method === 'GET') {
-    switch (req.url) {
-      case '/':
-        return res.end('Home');
-      case '/users':
-        return res.end(JSON.stringify(users));
-      default:
-        return res.end('no such page');
-    }
-  } else if (req.method === 'POST') {
-    switch (req.url) {
-      case '/users':
-        let jsonStr = '';
-        req.on('data', (chunk) => {
-          jsonStr += chunk;
-        });
-        req.on('end', () => {
-          const newUser = JSON.parse(jsonStr);
-          newUser.id = Date.now();
-          users.push(newUser);
-          res.end(JSON.stringify(users));
-        });
-        break;
-      default:
-        return res.end('no such page');
-    }
+app.get(
+  '/',
+  (req, res, next) => {
+    console.log('home');
+    req.test = 'magic';
+    //res.send('Error'); //- can stop req,res cycle
+    next();
+  },
+  (req, res) => {
+    console.log(req.test);
+    res.send('HOME');
   }
+);
+app.get('/users', (req, res) => {
+  res.send(users);
+});
+app.get('*', (req, res) => {
+  res.send('NOT FOUND');
 });
 
-const PORT = 3000;
-server.listen(PORT);
+app.listen(PORT, HOST, () => {
+  console.log(`Server started on ${HOST}:${PORT}`);
+});
