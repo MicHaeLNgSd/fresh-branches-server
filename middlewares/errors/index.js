dc97cb8a04918f5f2a5d6a00844c1fb2a1e5e8b8
MicHaeLNgSd <misha.grigorov2020@gmail.com>
2024-02-18 22:19:35 +0200

    Initial commit clear slate

--- middlewares/errors/index.js
+++
@@ -1,7 +0,0 @@
-module.exports.handleError1MW = async (err, req, res, next) => {
-  if (err === 'basic error') res.send(`ERROR1: ${err}`);
-  else next(err);
-};
-module.exports.handleError2MW = async (err, req, res, next) => {
-  res.send(`ERROR2: ${err}`);
-};
--- routers/index.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const userRouter = require('./userRouter');
-
-// роутер експрессу, містить тіж самі методи маршрутизації що і app
-const router = express.Router();
-router.use('/users', userRouter);
-
-module.exports = router;
